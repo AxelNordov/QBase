2e34b69ca95ecd855bbf67932623049265192a01
AxelNordov <iq@ua.fm>
2020-10-12 14:02:08 +0300

    init commit

---
+++ src/main/java/com/heroku/qbase/QBaseApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.qbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heroku/qbase/QBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.qbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
